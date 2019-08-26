0c31aeec7e8ef2088963f1161b60944fb2df514b
Dave Houlton <daveh@lunarg.com>
2019-08-26 16:49:00 -0600

    xroverlay: overlay skeleton wip

---
+++ XR_overlay_ext/xr_layer_interface.cpp
@@ -0,0 +1,145 @@
+#include "xr_overlay_dll.h"
+#include "xr_generated_dispatch_table.h"
+
+#include <assert.h>
+#include <string.h>
+
+const char *kOverlayLayerName = "XR_EXT_overlay_api_layer";
+
+static XrGeneratedDispatchTable *downchain = nullptr;
+
+#ifdef __cplusplus    // If used by C++ code, 
+extern "C" {          // we need to export the C interface
+#endif
+
+
+// Negotiate an interface with the loader 
+XR_OVERLAY_EXT_API XrResult Overlay_xrNegotiateLoaderApiLayerInterface(const XrNegotiateLoaderInfo *loaderInfo, 
+                                                    const char *layerName,
+                                                    XrNegotiateApiLayerRequest *layerRequest) 
+{
+    if (nullptr != layerName)
+    {
+        if (0 != strncmp(kOverlayLayerName, layerName, strnlen_s(kOverlayLayerName, XR_MAX_API_LAYER_NAME_SIZE)))
+        {
+            return XR_ERROR_INITIALIZATION_FAILED;
+        }
+    }
+
+    if (nullptr == loaderInfo || 
+        nullptr == layerRequest || 
+        loaderInfo->structType != XR_LOADER_INTERFACE_STRUCT_LOADER_INFO ||
+        loaderInfo->structVersion != XR_LOADER_INFO_STRUCT_VERSION || 
+        loaderInfo->structSize != sizeof(XrNegotiateLoaderInfo) ||
+        layerRequest->structType != XR_LOADER_INTERFACE_STRUCT_API_LAYER_REQUEST ||
+        layerRequest->structVersion != XR_API_LAYER_INFO_STRUCT_VERSION ||
+        layerRequest->structSize != sizeof(XrNegotiateApiLayerRequest) ||
+        loaderInfo->minInterfaceVersion > XR_CURRENT_LOADER_API_LAYER_VERSION ||
+        loaderInfo->maxInterfaceVersion < XR_CURRENT_LOADER_API_LAYER_VERSION ||
+        loaderInfo->minXrVersion < XR_MAKE_VERSION(0, 9, 0) || 
+        loaderInfo->minXrVersion >= XR_MAKE_VERSION(1, 1, 0))
+    {
+        return XR_ERROR_INITIALIZATION_FAILED;
+    }
+
+    layerRequest->layerInterfaceVersion = XR_CURRENT_LOADER_API_LAYER_VERSION;
+    layerRequest->layerXrVersion = XR_MAKE_VERSION(1, 0, 0);
+    layerRequest->getInstanceProcAddr = reinterpret_cast<PFN_xrGetInstanceProcAddr>(Overlay_xrGetInstanceProcAddr);
+    layerRequest->createApiLayerInstance = reinterpret_cast<PFN_xrCreateApiLayerInstance>(Overlay_xrCreateInstance);
+
+    return XR_SUCCESS;
+}
+
+XrResult Overlay_xrCreateInstance(const XrInstanceCreateInfo *info, XrInstance *instance) 
+{ 
+    // Layer initialization here
+    return XR_SUCCESS; 
+}
+
+XrResult Overlay_xrDestroyInstance(XrInstance instance) 
+{ 
+    // Layer cleanup here
+    return XR_SUCCESS; 
+}
+
+XrResult Overlay_xrCreateApiLayerInstance(const XrInstanceCreateInfo *info, const struct XrApiLayerCreateInfo *apiLayerInfo, XrInstance *instance) 
+{
+    assert(0 == strncmp(kOverlayLayerName, apiLayerInfo->nextInfo->layerName, strnlen_s(kOverlayLayerName, XR_MAX_API_LAYER_NAME_SIZE)));
+    assert(nullptr != apiLayerInfo->nextInfo);
+
+    // Copy the contents of the layer info struct, but then move the next info up by
+    // one slot so that the next layer gets information.
+    XrApiLayerCreateInfo local_api_layer_info = {};
+    memcpy(&local_api_layer_info, apiLayerInfo, sizeof(XrApiLayerCreateInfo));
+    local_api_layer_info.nextInfo = apiLayerInfo->nextInfo->next;
+
+    // Get the function pointers we need
+    PFN_xrGetInstanceProcAddr       pfn_next_gipa = apiLayerInfo->nextInfo->nextGetInstanceProcAddr;
+    PFN_xrCreateApiLayerInstance    pfn_next_cali = apiLayerInfo->nextInfo->nextCreateApiLayerInstance;
+
+    // Create the instance
+    XrInstance returned_instance = *instance;
+    XrResult result = pfn_next_cali(info, &local_api_layer_info, &returned_instance);
+    *instance = returned_instance;
+
+    // Create the dispatch table to the next levels
+    downchain = new XrGeneratedDispatchTable();
+    GeneratedXrPopulateDispatchTable(downchain, returned_instance, pfn_next_gipa);
+
+    // TBD where should the layer's dispatch table live? File global for now...
+
+    //std::unique_lock<std::mutex> mlock(g_instance_dispatch_mutex);
+    //g_instance_dispatch_map[returned_instance] = next_dispatch;
+
+    return result;
+}
+
+XrResult Overlay_xrCreateSwapchain(XrSession session, const  XrSwapchainCreateInfo *createInfo, XrSwapchain *swapchain) 
+{ 
+    return XR_ERROR_INITIALIZATION_FAILED;   // TBD
+    downchain->CreateSwapchain(session, createInfo, swapchain);
+}
+
+XrResult Overlay_xrBeginFrame(XrSession session, const XrFrameBeginInfo *info) 
+{ 
+    return XR_ERROR_INITIALIZATION_FAILED;   // TBD
+    downchain->BeginFrame(session, info);
+}
+
+XrResult Overlay_xrEndFrame(XrSession session, const XrFrameEndInfo *info) 
+{ 
+    return XR_ERROR_INITIALIZATION_FAILED;   // TBD
+    downchain->EndFrame(session, info);
+}
+
+XrResult Overlay_xrWaitFrame(XrSession session, const XrFrameWaitInfo *info, XrFrameState *state) 
+{ 
+    return XR_ERROR_INITIALIZATION_FAILED;   // TBD
+    downchain->WaitFrame(session, info, state);
+}
+
+XrResult Overlay_xrGetInstanceProcAddr(XrInstance instance, const char *name, PFN_xrVoidFunction *function) {
+  if (0 == strcmp(name, "xrGetInstanceProcAddr")) {
+    *function = reinterpret_cast<PFN_xrVoidFunction>(Overlay_xrGetInstanceProcAddr);
+  } else if (0 == strcmp(name, "xrCreateInstance")) {
+    *function = reinterpret_cast<PFN_xrVoidFunction>(Overlay_xrCreateInstance);
+  } else if (0 == strcmp(name, "xrDestroyInstance")) {
+    *function = reinterpret_cast<PFN_xrVoidFunction>(Overlay_xrDestroyInstance);
+  } else if (0 == strcmp(name, "xrCreateSwapchain")) {
+    *function = reinterpret_cast<PFN_xrVoidFunction>(Overlay_xrCreateSwapchain);
+  } else if (0 == strcmp(name, "xrBeginFrame")) {
+    *function = reinterpret_cast<PFN_xrVoidFunction>(Overlay_xrBeginFrame);
+  } else if (0 == strcmp(name, "xrEndFrame")) {
+    *function = reinterpret_cast<PFN_xrVoidFunction>(Overlay_xrEndFrame);
+  } else if (0 == strcmp(name, "xrWaitFrame")) {
+    *function = reinterpret_cast<PFN_xrVoidFunction>(Overlay_xrWaitFrame);
+  } else {
+    *function = nullptr;
+  }
+
+  return *function ? XR_SUCCESS : XR_ERROR_FUNCTION_UNSUPPORTED;
+}
+
+#ifdef __cplusplus
+}   // extern "C"
+#endif
---
+++ XR_overlay_ext/xr_overlay_dll.h
@@ -0,0 +1,55 @@
+#pragma once
+
+// Including SDKDDKVer.h defines the highest available Windows platform.
+#include <SDKDDKVer.h>
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+#include <windows.h>
+
+#include <openxr/openxr.h>
+#include <xr_dependencies.h>
+#include <loader_interfaces.h>
+
+
+//#if defined(__GNUC__) && __GNUC__ >= 4
+//#define XR_OVERLAY_EXT_API __attribute__((visibility("default")))
+//#elif defined(__clang__)
+//#define XR_OVERLAY_EXT_API __attribute__((visibility("default")))
+//#else // Windows
+//#define blah
+//#endif
+
+// Just VisStudio, for now
+#ifdef XR_OVERLAY_DLL_EXPORTS
+#define XR_OVERLAY_EXT_API __declspec(dllexport)
+#else
+#define XR_OVERLAY_EXT_API __declspec(dllimport)
+#endif
+
+#ifdef __cplusplus    // If used by C++ code, 
+extern "C" {          // we need to export the C interface
+#endif
+
+// Shared memory entry points
+XR_OVERLAY_EXT_API bool MapSharedMemory(UINT32 size);
+XR_OVERLAY_EXT_API bool UnmapSharedMemory();
+XR_OVERLAY_EXT_API void SetSharedMem(LPCWSTR lpszBuf);
+XR_OVERLAY_EXT_API void GetSharedMem(LPWSTR lpszBuf, DWORD cchSize);
+
+
+// Exported entry points
+XR_OVERLAY_EXT_API XrResult Overlay_xrCreateInstance(const XrInstanceCreateInfo*, XrInstance*);
+XR_OVERLAY_EXT_API XrResult Overlay_xrDestroyInstance(XrInstance);
+XR_OVERLAY_EXT_API XrResult Overlay_xrGetInstanceProcAddr(XrInstance, const char*, PFN_xrVoidFunction*);
+XR_OVERLAY_EXT_API XrResult Overlay_xrCreateSwapchain(XrSession, const  XrSwapchainCreateInfo*, XrSwapchain*);
+XR_OVERLAY_EXT_API XrResult Overlay_xrBeginFrame(XrSession, const XrFrameBeginInfo*);
+XR_OVERLAY_EXT_API XrResult Overlay_xrEndFrame(XrSession, const XrFrameEndInfo*);
+XR_OVERLAY_EXT_API XrResult Overlay_xrWaitFrame(XrSession, const XrFrameWaitInfo*, XrFrameState*);
+
+// Function used to negotiate an interface betewen the loader and a layer.
+XR_OVERLAY_EXT_API XrResult Overlay_xrNegotiateLoaderApiLayerInterface(const XrNegotiateLoaderInfo *li, const char *ln, XrNegotiateApiLayerRequest *lr);
+
+     
+#ifdef __cplusplus
+}
+#endif
