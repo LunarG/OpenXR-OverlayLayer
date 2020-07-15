4e72d8f22165fe143bf314896481666a7ccc22e5
Brad Grantham <brad@lunarg.com>
2020-07-15 16:42:00 -0600

    add boilerplate functions and make layer DLL link

---
+++ api-layer/overlays.cpp
@@ -0,0 +1,195 @@
+// Copyright (c) 2017-2020 The Khronos Group Inc.
+// Copyright (c) 2017-2019 Valve Corporation
+// Copyright (c) 2017-2020 LunarG, Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Author: Mark Young <marky@lunarg.com>
+// Author: Dave Houlton <daveh@lunarg.com>
+// Author: Brad Grantham <brad@lunarg.com>
+//
+
+#include "loader_interfaces.h"
+#include "platform_utils.hpp"
+#include "xr_generated_overlays.hpp"
+#include "xr_generated_dispatch_table.h"
+
+#include <openxr/openxr.h>
+
+#include <algorithm>
+#include <cctype>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <mutex>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+#if defined(__GNUC__) && __GNUC__ >= 4
+#define LAYER_EXPORT __attribute__((visibility("default")))
+#elif defined(_WIN32)
+#define LAYER_EXPORT __declspec(dllexport)
+#else
+#define LAYER_EXPORT
+#endif
+
+const char *kOverlayLayerName = "XR_EXT_overlay_api_layer";
+
+// Utility function to return an instance based on the generated dispatch table
+// pointer.
+XrInstance FindInstanceFromDispatchTable(XrGeneratedDispatchTable *dispatch_table)
+{
+    std::unique_lock<std::mutex> mlock(g_instance_dispatch_mutex);
+    XrInstance instance = XR_NULL_HANDLE;
+    for (auto it = g_instance_dispatch_map.begin(); it != g_instance_dispatch_map.end();) {
+        if (it->second == dispatch_table) {
+            instance = it->first;
+            break;
+        }
+    }
+    return instance;
+}
+
+XrResult OverlaysLayerXrCreateInstance(const XrInstanceCreateInfo * /*info*/, XrInstance * /*instance*/)
+{
+    return XR_SUCCESS;
+}
+
+
+XrResult OverlaysLayerXrCreateApiLayerInstance(const XrInstanceCreateInfo *info,
+        const struct XrApiLayerCreateInfo *apiLayerInfo, XrInstance *instance)
+{
+    PFN_xrGetInstanceProcAddr next_get_instance_proc_addr = nullptr;
+    PFN_xrCreateApiLayerInstance next_create_api_layer_instance = nullptr;
+    XrApiLayerCreateInfo new_api_layer_info = {};
+
+#if 0
+    gMainInstanceContext.savedRequestedExtensions.clear();
+    gMainInstanceContext.savedRequestedExtensions.insert(XR_EXTX_OVERLAY_EXTENSION_NAME);
+    gMainInstanceContext.savedRequestedExtensions.insert(info->enabledExtensionNames, info->enabledExtensionNames + info->enabledExtensionCount);
+
+    gMainInstanceContext.savedRequestedApiLayers.clear();
+    // Is there a way to query which layers are only downstream?
+    // We can't get to the functionality of layers upstream (closer to
+    // the app), so we can't claim all these layers are enabled (the remote
+    // app can't use these layers)
+    // gMainInstanceContext.savedRequestedApiLayers.insert(info->enabledApiLayerNames, info->enabledApiLayerNames + info->enabledApiLayerCount);
+#endif
+
+    // Validate the API layer info and next API layer info structures before we try to use them
+    if (!apiLayerInfo ||
+        XR_LOADER_INTERFACE_STRUCT_API_LAYER_CREATE_INFO != apiLayerInfo->structType ||
+        XR_API_LAYER_CREATE_INFO_STRUCT_VERSION > apiLayerInfo->structVersion ||
+        sizeof(XrApiLayerCreateInfo) > apiLayerInfo->structSize ||
+        !apiLayerInfo->nextInfo ||
+        XR_LOADER_INTERFACE_STRUCT_API_LAYER_NEXT_INFO != apiLayerInfo->nextInfo->structType ||
+        XR_API_LAYER_NEXT_INFO_STRUCT_VERSION > apiLayerInfo->nextInfo->structVersion ||
+        sizeof(XrApiLayerNextInfo) > apiLayerInfo->nextInfo->structSize ||
+        0 != strcmp("XR_APILAYER_LUNARG_api_dump", apiLayerInfo->nextInfo->layerName) ||
+        nullptr == apiLayerInfo->nextInfo->nextGetInstanceProcAddr ||
+        nullptr == apiLayerInfo->nextInfo->nextCreateApiLayerInstance) {
+        return XR_ERROR_INITIALIZATION_FAILED;
+    }
+
+    // Copy the contents of the layer info struct, but then move the next info up by
+    // one slot so that the next layer gets information.
+    memcpy(&new_api_layer_info, apiLayerInfo, sizeof(XrApiLayerCreateInfo));
+    new_api_layer_info.nextInfo = apiLayerInfo->nextInfo->next;
+
+    // Get the function pointers we need
+    next_get_instance_proc_addr = apiLayerInfo->nextInfo->nextGetInstanceProcAddr;
+    next_create_api_layer_instance = apiLayerInfo->nextInfo->nextCreateApiLayerInstance;
+
+    // Create the instance
+    XrInstance returned_instance = *instance;
+    XrResult result = next_create_api_layer_instance(info, &new_api_layer_info, &returned_instance);
+    *instance = returned_instance;
+
+    // Create the dispatch table to the next levels
+    auto *next_dispatch = new XrGeneratedDispatchTable();
+    GeneratedXrPopulateDispatchTable(next_dispatch, returned_instance, next_get_instance_proc_addr);
+
+    std::unique_lock<std::mutex> mlock(g_instance_dispatch_mutex);
+    g_instance_dispatch_map[returned_instance] = next_dispatch;
+
+    // CreateOverlaySessionThread();
+
+    return result;
+}
+
+XrResult OverlaysLayerXrDestroyInstance(XrInstance instance) {
+    std::unique_lock<std::mutex> mlock(g_instance_dispatch_mutex);
+    XrGeneratedDispatchTable *next_dispatch = nullptr;
+    auto map_iter = g_instance_dispatch_map.find(instance);
+    if (map_iter != g_instance_dispatch_map.end()) {
+        next_dispatch = map_iter->second;
+    }
+    mlock.unlock();
+
+    if (nullptr == next_dispatch) {
+        return XR_ERROR_HANDLE_INVALID;
+    }
+
+    next_dispatch->DestroyInstance(instance);
+
+    return XR_SUCCESS;
+}
+
+extern "C" {
+
+// Function used to negotiate an interface betewen the loader and an API layer.  Each library exposing one or
+// more API layers needs to expose at least this function.
+XrResult LAYER_EXPORT XRAPI_CALL xrNegotiateLoaderApiLayerInterface(const XrNegotiateLoaderInfo *loaderInfo,
+                                                                    const char* apiLayerName,
+                                                                    XrNegotiateApiLayerRequest *apiLayerRequest)
+{
+    if (apiLayerName)
+    {
+        if (0 != strncmp(kOverlayLayerName, apiLayerName, strnlen_s(kOverlayLayerName, XR_MAX_API_LAYER_NAME_SIZE)))
+        {
+            return XR_ERROR_INITIALIZATION_FAILED;
+        }
+    }
+
+    if (!loaderInfo ||
+        !apiLayerRequest ||
+        loaderInfo->structType != XR_LOADER_INTERFACE_STRUCT_LOADER_INFO ||
+        loaderInfo->structVersion != XR_LOADER_INFO_STRUCT_VERSION ||
+        loaderInfo->structSize != sizeof(XrNegotiateLoaderInfo) ||
+        apiLayerRequest->structType != XR_LOADER_INTERFACE_STRUCT_API_LAYER_REQUEST ||
+        apiLayerRequest->structVersion != XR_API_LAYER_INFO_STRUCT_VERSION ||
+        apiLayerRequest->structSize != sizeof(XrNegotiateApiLayerRequest) ||
+        loaderInfo->minInterfaceVersion > XR_CURRENT_LOADER_API_LAYER_VERSION ||
+        loaderInfo->maxInterfaceVersion < XR_CURRENT_LOADER_API_LAYER_VERSION ||
+        loaderInfo->maxInterfaceVersion > XR_CURRENT_LOADER_API_LAYER_VERSION ||
+        loaderInfo->maxApiVersion < XR_CURRENT_API_VERSION ||
+        loaderInfo->minApiVersion > XR_CURRENT_API_VERSION) {
+        return XR_ERROR_INITIALIZATION_FAILED;
+    }
+
+    apiLayerRequest->layerInterfaceVersion = XR_CURRENT_LOADER_API_LAYER_VERSION;
+    apiLayerRequest->layerApiVersion = XR_CURRENT_API_VERSION;
+    apiLayerRequest->getInstanceProcAddr = reinterpret_cast<PFN_xrGetInstanceProcAddr>(OverlaysLayerXrGetInstanceProcAddr);
+    apiLayerRequest->createApiLayerInstance = reinterpret_cast<PFN_xrCreateApiLayerInstance>(OverlaysLayerXrCreateApiLayerInstance);
+
+    return XR_SUCCESS;
+}
+
+}  // extern "C"
